0f23a95710b35c299cd1fc65bfda8bda131aa9ae
Olga Bursina <bursina.olya@yandex.ru>
2020-06-04 06:44:16 +0500

    initial commit

---
+++ IDZ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IDZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
